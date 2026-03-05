7a5169c53b9ca505ef872cb0997de9622c342da7
Eduardo Magno <eduardomagnosantosilva>
2026-03-05 04:10:50 -0300

    adicionado nova variável para o aplicativo tecnico

--- tecnico/urls.py
+++ tecnico/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
 from . import views
+from .views import salvar
 
 urlpatterns = [
-    path('', views.home, name = "home_aluno")
+    path('', views.home, name = "home_aluno"),
+    path('salvar', salvar, name = "salvar")
 ]
--- tecnico/views.py
+++ tecnico/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import tecnico
 
 # Create your views here.
 def home(request):
-    return render(request, "tecnico/home.html")
+    tecnicos = tecnico.objects.all()
+    
+    return render(request, "tecnico/home.html",{"tecnicos": tecnicos})
+
+def salvar(request):
+    nome = request.POST.get("nome")
+    matricula = request.POST.get("matricula")
+    tecnico.objects.create(nome = nome, matricula = matricula)
+    tecnicos = tecnico.objects.all()
+    
+    return render (request,"tecnico/home.html",{"tecnicos": tecnicos})
