a4b442fd8d69550e162b7f5e97be9618518adcf5
Eduardo Magno <eduardomagnosantosilva>
2026-03-10 10:06:39 -0300

    adicionado html base para outras páginas

--- projeto_final/settings.py
+++ projeto_final/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'projeto_final.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
