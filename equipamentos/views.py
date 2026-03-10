ecb7e28904a0d4f7e387ce6c19f4a8759782f5bb
Eduardo Magno <eduardomagnosantosilva>
2026-03-10 08:49:45 -0300

    resolvido problema do forms sem dados

--- equipamentos/views.py
+++ equipamentos/views.py
@@ -28,23 +28,19 @@ def editar_equip(request, id):
     
 def update_equip(request, id):
     
-    equipamento = get_object_or_404(Equipamento, id=id)
-    if request.method == "POST":
-        id_remoto = request.POST.get("id_remoto")
-        equipamento = Equipamento.objects.get(id=id)
-        
-        equipamento.tombo = request.POST.get("tombo")
-        equipamento.tipo = request.POST.get("tipo")
-        equipamento.descricao = request.POST.get("descricao")
-        equipamento.setor = request.POST.get("setor")
-        equipamento.usuario = request.POST.get("usuario")
-    
-        equipamento.id_remoto = int(id_remoto) if id_remoto else None
-        equipamento.save()
-        
-        return redirect(equipamentos)
-    
-    return render(request,"equipamentos.html",{"equipamento":equipamento})
+    id_remoto = request.POST.get("id_remoto")
+    equipamento = Equipamento.objects.get(id=id)
+
+    equipamento.tombo = request.POST.get("tombo")
+    equipamento.tipo = request.POST.get("tipo")
+    equipamento.descricao = request.POST.get("descricao")
+    equipamento.setor = request.POST.get("setor")
+    equipamento.usuario = request.POST.get("usuario")
+
+    equipamento.id_remoto = int(id_remoto) if id_remoto else None
+    equipamento.save()
+
+    return redirect(equipamentos)
     
 def deletar_equip(request, id):
     equipamento = Equipamento.objects.get(id=id)
